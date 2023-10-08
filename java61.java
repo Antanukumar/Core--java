e67661af675ab806a48c48dce6bf08f36d56e94c
Antanu kumar <antanukumar9@gmail.com>
2023-10-08 22:44:23 +0530

    upadate core java

---
+++ java61.java
@@ -0,0 +1,8 @@
+public class java61 {
+    public static void main(String[] args) {
+        for(;;){
+            
+        }
+    }
+    
+}
---
+++ java62.java
@@ -0,0 +1,13 @@
+public class java62 {
+    public static void main(String[] args) {
+        for (int i=5;i!=0; i-=2)
+        {
+            System.out.println(i);
+        }
+        int x=5;
+
+        while(x==5){
+            System.out.println("in the loop");
+        }
+    }
+}
